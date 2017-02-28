24559bbac8c99e9d4d6d3dea16722d7393dec958
Deyan <dublinstand@gmail.com>
2017-02-28 17:18:36 +0000

    modified:   .idea/workspace.xml 	deleted:    out/production/SoftUniJava/ConditionalStatements.class 	deleted:    out/production/SoftUniJava/FloatingPointTypes.class 	modified:   out/production/SoftUniJava/org/b/syntax/_01_RectangleArea.class 	new file:   out/production/SoftUniJava/org/b/syntax/_02_TriangleArea$Point.class 	new file:   out/production/SoftUniJava/org/b/syntax/_02_TriangleArea.class 	new file:   out/production/SoftUniJava/org/b/syntax/demos/ConditionalStatements.class 	new file:   out/production/SoftUniJava/org/b/syntax/demos/FloatingPointTypes.class 	modified:   src/org/b/syntax/_01_RectangleArea.java 	new file:   src/org/b/syntax/_02_TriangleArea.java

---
+++ src/org/b/syntax/_02_TriangleArea.java
@@ -0,0 +1,40 @@
+package org.b.syntax;
+
+//Write a program that enters 3 points in the plane (as integer x and y coordinates), calculates and prints the area of the triangle
+//        composed by these 3 points. Round the result to a whole number. In case the three points do not form a triangle,
+//        print "0" as result.
+
+import java.util.Scanner;
+
+public class _02_TriangleArea {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        int x = scanner.nextInt();
+//        int y = scanner.nextInt();
+        Point a = new Point(scanner.nextInt(), scanner.nextInt());
+        Point b = new Point(scanner.nextInt(), scanner.nextInt());
+        Point c = new Point(scanner.nextInt(), scanner.nextInt());
+
+        CalculateAndPrintAreaOfTriangle(a, b, c);
+
+    }
+
+    private static void CalculateAndPrintAreaOfTriangle(Point a, Point b, Point c) {
+        double area = Math.abs(((a.X*(b.Y - c.Y) + b.X*(c.Y - a.Y) +c.X*(a.Y - b.Y))/2 ));
+        System.out.print((int)area);
+    }
+
+    public static class Point{
+        private int X;
+        private int Y;
+
+        public Point(int x, int y) {
+            X = x;
+            Y = y;
+        }
+
+    }
+}
+
+
