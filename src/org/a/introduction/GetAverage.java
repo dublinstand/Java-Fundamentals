69e47187c05ba95a6dbac3379b451a34639dd5ce
Deyan <dublinstand@gmail.com>
2017-02-28 13:53:28 +0000

    Added exercise

---
+++ src/org/a/introduction/GetAverage.java
@@ -0,0 +1,8 @@
+package org.a.introduction;
+
+//Create a method that finds the average of three numbers. Read in internet about java methods. Check the naming conventions.
+//        Invoke your method and test it. Format the output to two digits after the decimal separator. The placeholder is %.2f
+
+public class GetAverage {
+
+}
