bf7fb8627dee976be98c6aebcfe05e0cbc27b730
Deyan <dublinstand@gmail.com>
2017-03-01 14:48:47 +0000

    modified:   .idea/workspace.xml 	new file:   out/production/SoftUniJava/org/b/syntax/_03_CalculateExpression.class 	new file:   src/org/b/syntax/_03_CalculateExpression.java 	new file:   src/org/b/syntax/_04_RandomizeNumbersFromNtoM.java

---
+++ src/org/b/syntax/_04_RandomizeNumbersFromNtoM.java
@@ -0,0 +1,17 @@
+package org.b.syntax;
+
+import java.util.Scanner;
+
+//Write a program that takes as input two integers N and M,
+//        and randomizes the numbers between them. Note that M may be smaller than or equal to N.
+
+public class _04_RandomizeNumbersFromNtoM {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner((System.in));
+
+
+
+    }
+
+}
