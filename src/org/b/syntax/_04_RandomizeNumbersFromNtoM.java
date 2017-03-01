f8a15991d797992309e3f1345af0e5d22d94fd81
Deyan <dublinstand@gmail.com>
2017-03-01 15:34:51 +0000

    modified:   .idea/workspace.xml 	modified:   SoftUniJava.iml 	new file:   google-collections-1.0-rc1.jar 	modified:   out/production/SoftUniJava/org/b/syntax/_03_CalculateExpression.class 	new file:   out/production/SoftUniJava/org/b/syntax/_04_RandomizeNumbersFromNtoM.class 	modified:   out/production/SoftUniJava/org/w/others/oopinjava/FunctionalProgramming.class 	modified:   out/production/SoftUniJava/org/w/others/oopinjava/StreamsOfElements.class 	modified:   out/production/SoftUniJava/org/w/others/oopinjava/shapes/SortingShapes.class 	modified:   src/org/b/syntax/_04_RandomizeNumbersFromNtoM.java

--- src/org/b/syntax/_04_RandomizeNumbersFromNtoM.java
+++ src/org/b/syntax/_04_RandomizeNumbersFromNtoM.java
@@ -1,5 +1,7 @@
 package org.b.syntax;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 //Write a program that takes as input two integers N and M,
@@ -10,8 +12,32 @@ public class _04_RandomizeNumbersFromNtoM {
     public static void main(String[] args) {
         Scanner scanner = new Scanner((System.in));
 
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
 
+        int numMin = Math.min(num1, num2);
+        int numMax = Math.max(num1, num2);
 
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = numMin; i <= numMax; i ++){
+            list.add(i);
+        }
+
+        list = RandomizeNumbers(list);
+
+        System.out.println(String.join(", ", list.toString()));
+    }
+
+    private static  ArrayList<Integer> RandomizeNumbers(ArrayList<Integer> list) {
+        Random rand = new Random();
+
+        for(int i = 0; i < list.size(); i ++){
+            int firstNum = list.get(i);
+            list.remove(i);
+            list.add(rand.nextInt(list.size()-1), firstNum);
+        }
+        return  list;
     }
 
 }
